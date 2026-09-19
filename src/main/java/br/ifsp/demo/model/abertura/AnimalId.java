166767dd8af6a1c8ce511549e9fc479d8410b518
Kaique Galera <kaiquegalera@gmail.com>
2026-09-19 20:35:32 -0300

    feat: cria objeto de valor AnimalId

---
+++ src/main/java/br/ifsp/demo/model/abertura/AnimalId.java
@@ -0,0 +1,28 @@
+package br.ifsp.demo.model.abertura;
+
+import java.util.Objects;
+import java.util.UUID;
+
+public class AnimalId {
+    private final UUID id;
+
+    public AnimalId(UUID id) {
+        this.id = id;
+    }
+
+    public static AnimalId novo(){
+        return new AnimalId(UUID.randomUUID());
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (o == null || getClass() != o.getClass()) return false;
+        AnimalId animalId = (AnimalId) o;
+        return Objects.equals(id, animalId.id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(id);
+    }
+}
