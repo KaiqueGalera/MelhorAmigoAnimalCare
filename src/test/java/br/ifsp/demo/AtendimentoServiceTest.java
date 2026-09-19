ee8013d24fd60c887cfe8329f43c4b5940c15194
Kaique Galera <kaiquegalera@gmail.com>
2026-09-19 20:13:32 -0300

    test: cria teste de abertura de Pronto atendimento (red)

---
+++ src/test/java/br/ifsp/demo/AtendimentoServiceTest.java
@@ -0,0 +1,21 @@
+package br.ifsp.demo;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.AssertionsForClassTypes.assertThat;
+
+class AtendimentoServiceTest {
+    private InMemoryAtendimentoRepository repository;
+    private AtendimentoService atendimentoService;
+
+    @Test
+    void devoAbrirProntoAtendimentoComSucesso(){
+        AnimalId animalId = AnimalId.novo();
+        repository = new InMemoryAtendimentoRepository();
+        service = new AtendimentoService(repository);
+        Atendimento atendimento = service.abrirProntoAtendimento;
+
+        assertThat(atendimento.getStatus().isEqualTo(StatusAtendimentos.EM_ANDAMENTO));
+        assertThat(atendimento.getAgendamento()).isNull();
+    }
+}
