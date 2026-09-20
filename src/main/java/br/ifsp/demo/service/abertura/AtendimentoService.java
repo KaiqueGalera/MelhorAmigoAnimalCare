da060085c942d5653da8a913e3924e6c8cb8ab30
Kaique Galera <kaiquegalera@gmail.com>
2026-09-19 21:26:43 -0300

    "feat: cria AtendimentoService"

--- src/main/java/br/ifsp/demo/service/abertura/AtendimentoService.java
+++ src/main/java/br/ifsp/demo/service/abertura/AtendimentoService.java
@@ -1,4 +1,20 @@
 package br.ifsp.demo.service.abertura;
 
+import br.ifsp.demo.model.abertura.AnimalId;
+import br.ifsp.demo.model.abertura.Atendimento;
+import br.ifsp.demo.repository.InMemoryAtendimentoRepository;
+
 public class AtendimentoService {
+    private final InMemoryAtendimentoRepository repository;
+
+    public AtendimentoService(InMemoryAtendimentoRepository repository) {
+        this.repository = repository;
+    }
+
+    public Atendimento abrirProntoAtendimento(AnimalId animalId){
+        Atendimento atendimento = Atendimento.abrirProntoAtendimento(animalId);
+        repository.salvar(atendimento);
+
+        return atendimento;
+    }
 }
