5d8e8d45339973c86e910abe5b067e8d4d1500d3
Kaique Galera <kaiquegalera@gmail.com>
2026-09-19 21:27:42 -0300

    "feat: cria InMemoryAtendimentoRepository"

--- src/main/java/br/ifsp/demo/repository/InMemoryAtendimentoRepository.java
+++ src/main/java/br/ifsp/demo/repository/InMemoryAtendimentoRepository.java
@@ -1,6 +1,14 @@
 package br.ifsp.demo.repository;
 
+import br.ifsp.demo.model.abertura.Atendimento;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class InMemoryAtendimentoRepository {
+    private final List<Atendimento> atendimentos = new ArrayList<>();
 
-
+    public void salvar(Atendimento atendimento){
+        atendimentos.add(atendimento);
+    }
 }
