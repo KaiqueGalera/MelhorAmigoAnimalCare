1ababfc45f96e0b75b4a142a8a1ff3ec4c326c60
Kaique Galera <kaiquegalera@gmail.com>
2026-09-21 23:11:14 -0300

    refactor(C0101): adiciona preparação e padroniza os nomes

--- src/test/java/br/ifsp/demo/AtendimentoServiceTest.java
+++ src/test/java/br/ifsp/demo/AtendimentoServiceTest.java
@@ -5,6 +5,7 @@ import br.ifsp.demo.model.abertura.Atendimento;
 import br.ifsp.demo.model.abertura.StatusAtendimento;
 import br.ifsp.demo.repository.InMemoryAtendimentoRepository;
 import br.ifsp.demo.service.abertura.AtendimentoService;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -15,12 +16,16 @@ class AtendimentoServiceTest {
     private InMemoryAtendimentoRepository repository;
     private AtendimentoService service;
 
-    @Test
-    @DisplayName("# 2 Devo abrir pronto atendimento para animal sem agendamento prévio")
-    void devoAbrirProntoAtendimentoComSucesso(){
-        AnimalId animalId = AnimalId.novo();
+    @BeforeEach()
+    void prepara(){
         repository = new InMemoryAtendimentoRepository();
         service = new AtendimentoService(repository);
+    }
+
+    @Test
+    @DisplayName("# C0101 Devo abrir pronto atendimento para animal sem agendamento prévio")
+    void devoAbrirProntoAtendimentoComSucesso(){
+        AnimalId animalId = AnimalId.novo();
         Atendimento atendimento = service.abrirProntoAtendimento(animalId);
 
         assertThat(atendimento.getStatus()).isEqualTo(StatusAtendimento.EM_ANDAMENTO);
@@ -28,13 +33,10 @@ class AtendimentoServiceTest {
     }
 
     @Test
-    @DisplayName("# 3 Não devo abrir pronto atendimento quando animal já estiver em atendimento")
+    @DisplayName("# C0102 Não devo abrir pronto atendimento quando animal já estiver em atendimento")
     void naoDevoAbrirProntoAtendimento(){
         AnimalId animalId = AnimalId.novo();
-        repository = new InMemoryAtendimentoRepository();
-        service = new AtendimentoService(repository);
         service.abrirProntoAtendimento(animalId);
-
 
         assertThrows(IllegalStateException.class, ()-> service.abrirProntoAtendimento(animalId));
     }
